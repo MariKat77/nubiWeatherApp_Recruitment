f4999a6366c823a1efc3a9cf759e2282f511b1b1
Mario <mario.1999.mk@gmail.com>
2024-06-24 12:12:26 +0200

    Added tests

--- spring + java/nubiweather/src/test/java/com/nubisoft/nubiweather/NubiweatherApplicationTests.java
+++ spring + java/nubiweather/src/test/java/com/nubisoft/nubiweather/NubiweatherApplicationTests.java
@@ -1,13 +1,45 @@
 package com.nubisoft.nubiweather;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nubisoft.nubiweather.models.ForecastCityRecord;
+import com.nubisoft.nubiweather.models.WeatherRecord;
+import com.nubisoft.nubiweather.service.WeatherService;
 
 @SpringBootTest
 class NubiweatherApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WeatherService weatherService;
 
+    @Test
+    void testGetRealtimeWeather() {
+
+        WeatherRecord response = weatherService.getRealtimeWeather();
+        assertNotNull(response);
+        assertEquals(2, response.weatherRecord().size());
+        String gliwiceName = response.weatherRecord().get(0).location().name();
+        assertNotNull(gliwiceName);
+        assertEquals("Gliwice", gliwiceName);
+        String hamburgName = response.weatherRecord().get(1).location().name();
+        assertNotNull(hamburgName);
+        assertEquals("Hamburg", hamburgName);
+    }
+
+    @Test
+    public void testGetForecastWeather() {
+        ForecastCityRecord response = weatherService.getForecastWeather();
+        assertNotNull(response);
+        assertEquals(2, response.forecastRecord().size());
+        String gliwiceForecastName = response.forecastRecord().get(0).location().name();
+        assertNotNull(gliwiceForecastName);
+        assertEquals("Gliwice", gliwiceForecastName);
+        String hamburgForecastName = response.forecastRecord().get(1).location().name();
+        assertNotNull(hamburgForecastName);
+        assertEquals("Hamburg", hamburgForecastName);
+    }
 }
